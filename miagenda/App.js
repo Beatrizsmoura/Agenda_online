59c6f2b1c402009638ab09d9de7eb07690ae37e0
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-07-25 16:52:09 -0300

    update tela inicial

--- miagenda/App.js
+++ miagenda/App.js
@@ -1,74 +1,27 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet, SafeAreaView, StatusBar, TouchableOpacity, FlatList} from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import TaskList from './src/components/TaskList';
+
+import Home from './src/pages/Home'
 
 // View é a mesma coisa que DIV
 export default function App(){
-
-    const [task, setTask] = useState([
-        { key: 1, task: 'Comprar pão'},
-        { key: 2, task: 'Dar boa noite'},
-        { key: 3, task: 'Enganar Trouxa'},
-    ])
-
     return(
         <SafeAreaView style={styles.container}>
-            <StatusBar backgroundColor="#171d31" barStyle="ligh-content" />
-            <View style={styles.content}>
-                <Text style={styles.title}>Tarefas</Text>
+            <View>
+                <Home></Home>
             </View>
 
-            <FlatList
-            marginHorizontal={10}
-            showsVerticalScrollIndicator={false}
-            data={task} // Data
-            keyExtractor={ (item) => String(item.key)} // Key
-            renderItem= { ({item}) => <TaskList data={item} />} // Render do item
-            />
-
-
-        <TouchableOpacity style={styles.fab}>
-            <Ionicons name="ios-add" size={40} color="#fff"/>
-        </TouchableOpacity>
-            
         </SafeAreaView>
+        
     )
 }
 
+
 const styles = StyleSheet.create({
     container: {
+        width: '100vw',
+        height: '100vh',
         flex: 1,
-        backgroundColor: '#171d31'
-    },
-
-    title: {
-        marginTop: 10,
-        paddingBottom: 10,
-        fontSize: 25,
-        textAlign: 'center',
-        color: '#fff'
-
-    },
-
-    fab: {
-        position: 'absolute',
-        width: 60,
-        height: 60,
-        backgroundColor: '#0094ff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 30,
-        right: 25,
-        bottom: 25,
-        elevation: 2,
-        zIndex: 9,
-        shadowColor: '#000',
-        shadowOpacity: 0.2,
-        shadowOffset: {
-            width: 1,
-            height: 3,
-        }
-
+        backgroundColor: 'background: linear-gradient(180deg, rgba(23, 98, 255, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #FDFEFF;',
     }
 })
