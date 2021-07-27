d60bfb5f8df1dd93cda6004ebcda43e0a1076503
Jeniffer Macena de Souza <61254473+jenmacena19@users.noreply.github.com>
2021-07-26 20:18:50 -0400

    mix

---
+++ miagenda/src/pages/Header/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import { View, Image, Text, StyleSheet } from "react-native";
+import { RectButton, BorderlessButton } from "react-native-gesture-handler";
+
+import { styles } from "./styles";
+
+import BackPng from "../../assets/back.png";
+
+export default unction Header() {
+    return (
+        <View style={styles.container}>
+            <BorderlessButton>
+                <Image
+                    source={BackPng}
+                />
+            </BorderlessButton>
+
+            <Text style={styles.dia}>Dia 22</Text>
+            <RectButton style={styles.buttonSecondary}>
+                <Text>Teste</Text>
+            </RectButton>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 63,
+        flexDirection: 'row',
+        marginTop: getStatusBarHeight(),
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 26
+    },
+    dia: {
+        fontFamily: 'Poppins_600SemiBold',
+        fontSize: 24,
+        lineHeight: 36
+    },
+    buttonSecondary: {
+        opacity: 0
+    }
+});
